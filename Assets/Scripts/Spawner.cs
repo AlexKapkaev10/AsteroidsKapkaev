6bf7aa7331d5ab34bbb66f20f8ec63f3108af320
chivoda10 <chivoda10@gmail.com>
2020-02-25 18:36:07 +0400

    upgreade respawn enemies

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,13 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    private float delaySpawn;
+    public float minDelay;
+    public float maxDelay;
+
+    private bool isSpawn;
     //ссылка на префаб
     public GameObject myPrefab;
 
     void Start()
     {
+        delaySpawn = Random.Range(minDelay, maxDelay);
         //создание астероида в точке SpawnAsteroid на сцене
+        //GameObject enemy = Instantiate(myPrefab, transform.position, transform.rotation);
+    }
+
+    private void Update()
+    {
+        if (!isSpawn)
+            StartCoroutine(RepeatingSpawner());
+    }
+
+    IEnumerator RepeatingSpawner()
+    {
+        isSpawn = true;
+        delaySpawn = Random.Range(minDelay, maxDelay);
+        yield return new WaitForSeconds(delaySpawn);
         GameObject enemy = Instantiate(myPrefab, transform.position, transform.rotation);
+        isSpawn = false;
     }
 }
